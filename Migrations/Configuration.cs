06d3260addc4011f277ecebfd146f79371b78e18
Lin <1159148810@qq.com>
2018-10-06 21:24:22 -0500

    first commit

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,65 @@
+﻿namespace WebApplication6.Migrations
+{
+    using WebApplication6.Models;
+
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<WebApplication6.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(WebApplication6.Models.ApplicationDbContext context)
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+            if (!context.Roles.Any(p => p.Name == "Admin"))
+            {
+                var role = new IdentityRole("Admin");
+
+                roleManager.Create(role);
+            }
+
+            if (!context.Roles.Any(p => p.Name == "Project Manager"))
+            {
+                roleManager.Create(new IdentityRole("Project Manager"));
+            }
+
+            if (!context.Roles.Any(p => p.Name == "Developer"))
+            {
+                roleManager.Create(new IdentityRole("Developer"));
+            }
+
+            if (!context.Roles.Any(p => p.Name == "Submitter"))
+            {
+                roleManager.Create(new IdentityRole("Submitter"));
+            }
+
+            ApplicationUser adminUser;
+
+            if (!context.Users.Any(p => p.UserName == "admin@bugtracker.com"))
+            {
+                adminUser = new ApplicationUser();
+                adminUser.Email = "admin@bugtracker.com";
+                adminUser.UserName = "admin@bugtracker.com";
+                userManager.Create(adminUser, "Password-1");
+            }
+            else
+            {
+                adminUser = context.Users.First(p => p.UserName == "admin@bugtracker.com");
+            }
+
+            if (!userManager.IsInRole(adminUser.Id, "Admin"))
+            {
+                userManager.AddToRole(adminUser.Id, "Admin");
+            }
+        }
+    }
+}
---
+++ Models/Classes/Project.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication6.Models.Classes
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Project()
+        {
+            Users = new HashSet<ApplicationUser>();
+        }
+        public ICollection<ApplicationUser> Users { get; set;}
+    }
+}
+
+
